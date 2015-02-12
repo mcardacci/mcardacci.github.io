bf74c06fd56fc14acb6efbec5adaab3d4ccc1adb
Marc Cardacci <marc.cardacci@gmail.com>
2015-02-11 21:26:07 -0500

    update blog

---
+++ JS/blog-template.js
@@ -0,0 +1,8 @@
+ // $(document).ready(function() {
+//
+  // $(".nav-icons").hover(function(){
+      // $(this).find(".nav-icons")animate({opacity:0})
+          // .queue(function(){$(this).text("new text"); $(this).dequeue()})
+          // .animate({opacity:1});
+  // });
+// });
