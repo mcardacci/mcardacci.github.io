ff704e980f0a2fd705dd7506a19059445c8d9d9e
Ace Burgess <thankace@gmail.com>
2015-03-28 22:16:22 -0400

    Add cheatsheet files

---
+++ cheatsheet/main.js
@@ -0,0 +1,23 @@
+var header = document.getElementById("headerJS");
+header.innerHTML =
+"<a href='main.html'>SQL Cheat Sheet</a>";
+
+var subHeader = document.getElementById("subHeaderJS");
+subHeader.innerHTML =
+"By Marco Cardacci & Ace Burgess";
+
+var navigationBar = document.getElementById("navigationJS");
+navigationBar.innerHTML =
+"<ul>" +
+"  <a href='changing_output.html'><li>CHANGING OUTPUT FORMATS IN SQLITE</li></a>" +
+"  <a href='create_database.html'><li>CREATE DATABASE</li></a>" +
+"  <a href='create_table.html'><li>CREATE TABLE</li></a>" +
+"  <a href='select.html'><li>SELECT</li></a>" +
+"  <a href='update.html'><li>UPDATE</li></a>" +
+"  <a href='delete.html'><li>DELETE</li></a>" +
+"  <a href='insert_into.html'><li>INSERT INTO</li></a>" +
+"  <a href='alter.html'><li>ALTER TABLE</li></a>" +
+"  <a href='drop_table.html'><li>DROP TABLE</li></a>" +
+"  <a href='create_index.html'><li>CREATE INDEX</li></a>" +
+"  <a href='drop_index.html'><li>DROP INDEX</li></a>" +
+"</ul>";
