9ed8d27e7c5d1254db168a49633066f59ac94803
mcardacci <marc.cardacci@gmail.com>
2015-09-09 17:36:06 -0400

    breaks item push at length 5

--- d3Form.js
+++ d3Form.js
@@ -6,10 +6,15 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      var myArr = JSON.parse(xmlhttp.responseText);
-      console.log(myArr);
-    } else {
-      console.log("Something went terribly wrong.")
+      var items = JSON.parse(xmlhttp.responseText).items;
+      var itemsArray = [];
+      var count = 0
+      while (count <= items.length-1) {
+        itemsArray.push(items[count].login);
+        count++;
+        if (count === 4) break;
+      }
+      console.log(itemsArray);
     }
   }
   var params = document.getElementById("myForm").elements[0].value;
