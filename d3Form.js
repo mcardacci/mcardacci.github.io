8d7120426f09f5c1635b1c5a33b9acf0f2e21cd6
mcardacci <marc.cardacci@gmail.com>
2015-09-09 20:13:56 -0400

    adds other field in chart, ensures lowercase edges, and extra cdn

--- d3Form.js
+++ d3Form.js
@@ -12,9 +12,18 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
       while (count <= items.length-1) {
         itemsArray.push(items[count].login);
         count++;
-        if (count === 4) break;
+        if (count === 5) break;
       }
       console.log(itemsArray);
+      //creates a histogram of inside barData
+      var xAxisLabels = "abcdefghijklmnopqrstuvwxyz".split("");
+      var barData = [];
+      var loginNames = itemsArray.join("").toLowerCase();
+      for (var j = 0; j < xAxisLabels.length; j++) {
+        barData.push({ letter: xAxisLabels[j], frequency: (loginNames.split(xAxisLabels[j]).length - 1)});
+      }
+      barData.push({ letter: "other", frequency: loginNames.match(/([^a-z])/g).length});
+      renderChart(barData);
     }
   }
   var params = document.getElementById("myForm").elements[0].value;
@@ -22,3 +31,80 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
   xmlhttp.send();
 });
 
+
+
+
+// D3 chart...........................................
+function renderChart(data) {
+  var margin = {top: 40, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var formatCount = d3.format("0");
+
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .tickFormat(formatCount);
+
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong>Count:</strong> <span style='color:red'>" + d.frequency + "</span>";
+    })
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.call(tip);
+
+//here it calls the tsv that you don't have
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Count");
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.frequency); })
+        .attr("height", function(d) { return height - y(d.frequency); })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+
+
+  function type(d) {
+    d.frequency = +d.frequency;
+    return d;
+  }
+};
